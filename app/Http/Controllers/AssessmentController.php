1d08c6483d8115c7e56d080a6bd718681bf65be0
salah-jr <mohamed.salah21500@gmail.com>
2022-03-04 22:32:57 +0200

    finish part one

---
+++ app/Http/Controllers/AssessmentController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class AssessmentController extends Controller
+{
+    public function problemOne(): int
+    {
+        $arr = request('arr_values');
+        $posArr = array();
+        for ($i = 0; $i < sizeof($arr); $i++) {
+            if ($arr[$i] > 0) {
+                $posArr[] += $arr[$i];
+            }
+        }
+        if (!empty($posArr)) {
+            $i = min($posArr);
+            while (in_array($i, $posArr)) {
+                $i++;
+            }
+            return $i;
+        }
+        return 1;
+    }
+
+    public function problemTwo(): int
+    {
+        $start = request('start');
+        $end = request('end');
+
+        $count = 0;
+        for ($i = $start; $i <= $end; $i++) {
+            if (!($i % 5 == 0 && $i % 10 != 0)) {
+                $count++;
+            }
+        }
+        return $count;
+    }
+
+    public function problemThree(): int
+    {
+        $arr = request('arr_values');
+        $result = 0;
+
+        for ($i = 0; $i < sizeof($arr); $i++) {
+            $result = $result ^ $arr[$i];
+        }
+        return $result;
+    }
+
+    public function problemFour()
+    {
+        $str = request('input_string');
+        $str = strtoupper($str);
+
+        // convert string into array of characters
+        $chars = str_split($str);
+
+        // Convert each character into its alphabetic position.
+        $positions = array();
+        foreach ($chars as $c) {
+            $positions[] += ord($c) - 64;
+        }
+
+        // In case the string is containing one letter
+        $result = $positions[0];
+
+        // In case the string is containing more than one letter
+        if (sizeof($positions) >= 2) {
+            $result = 0;
+            $i = 0;
+            while (isset($positions[$i + 1])) {
+                $result += $positions[$i] * 26 + $positions[$i + 1];
+                if (($i + 2) == sizeof($positions) - 1) {
+                    return $result * 26 + $positions[$i + 2];
+                }
+                $i++;
+            }
+        }
+        return $result;
+    }
+
+    public function createOrder()
+    {
+        // To implement later....
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AssessmentController;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
-|--------------------------------------------------------------------------
+|---------------------------------ee-----------------------------------------
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AssessmentController::class)->group(function () {
+    Route::prefix('part-one')->group(function () {
+        Route::post('problem-one', 'problemOne');
+        Route::get('problem-two', 'problemTwo');
+        Route::post('problem-three', 'problemThree');
+        Route::post('problem-four', 'problemFour');
+    });
+    Route::post('part-two/orders/create', 'createOrder');
 });
